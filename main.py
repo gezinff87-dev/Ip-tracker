690edbddc984be2f7c6fe7cf3b041eb0166f0a4f
! lzz <gezinff87@gmail.com>
2026-04-05 13:52:11 -0300

    Rename VPN option in menu from Proton to Free

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ def main():
         print(CYAN + "[1] Consultar URL")
         print("[2] Consultar IP")
         print("[3] Ver Meu IP (público e local)")
-        print("[4] Abrir VPN (Proton VPN)")
+        print("[4] Ativar VPN (VPN Free)")
         print("[5] Verificar mudança de IP")
         print("[6] Sair/exit\n" + RESET)
 
